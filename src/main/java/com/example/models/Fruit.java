e563745e9892cc4879f269c2a0df06a1680b8115
BenArayathel <ben.arayathel@gmail.com>
2021-09-08 10:38:47 -0400

    implementing my fruit object

--- src/main/java/com/example/models/Fruit.java
+++ src/main/java/com/example/models/Fruit.java
@@ -1,5 +1,31 @@
 package com.example.models;
 
 public class Fruit {
+	
+	private int id;
+	private String name;
+	public Fruit(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Fruit() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
